e64b0d51f4f6760c334ba68cd118484b14437606
Andrew <andrew@MacBook-Pro-Pavel.local>
2020-05-06 18:05:40 +0300

    Changes in pick_A_kard file

--- pick_a_kard.rb
+++ pick_a_kard.rb
@@ -1,4 +1,4 @@
 values = ['2','3','4','5','6','7','8','9','10','J','D','K']
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
+suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']# Масти
 
 puts "#{values.sample} of #{suits.sample}"
